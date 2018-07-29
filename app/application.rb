3dcf07831bf134e856a999c74020682f2ca8c378
Heidi Engstrom <hengstrom25@gmail.com>
2018-07-29 17:21:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
  
     if req.path.match(/items/)
       item.name = req.path.split("/items/").last
-      item = @@items.find {|i| i.
+      item = @@items.find {|i| i.name == item.name}
         if items
       resp.write "#{items.price}"
     else
