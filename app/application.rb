aa1737de67139624854d80b47f2aa9c0f9f5a42a
Heidi Engstrom <hengstrom25@gmail.com>
2018-07-29 17:22:56 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     req = Rack::Request.new(env)
  
     if req.path.match(/items/)
-      item.name = req.path.split("/items/").last
+      item_name = req.path.split("/items/").last
       item = @@items.find {|i| i.name == item.name}
         if item
         resp.write item.price
